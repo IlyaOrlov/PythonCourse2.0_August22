1c3168bdd213fb65901494d2d66364d07f15edf5
SafonovaEkaterina <doinikova-ekaterina@yandex.ru>
2022-08-31 15:09:30 +0300

    change

--- Practice/esafonova/task1.py
+++ Practice/esafonova/task1.py
@@ -1,4 +1,3 @@
 name = input("Введите имя: ")
 fullname = input("А теперь введите фамилию: ")
-age = input("Укажите возраст: ")
-print(f"Вас зовут:  {name} {fullname} \nВаш возраст: {age}")
+print(f"Echo:  {name} {fullname}")
