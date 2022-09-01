ff2168342991f39dbdb7f19d2b9a136405e238fc
NazGuJl <lis-jobs@yandex.ru>
2022-09-01 16:23:23 +0300

    day_2_tskak_1_2_3

---
+++ Practice/LukichevI/day_2_task_1.py
@@ -0,0 +1,12 @@
+# 1.Расположить инструкции программы для вычисления площади круга в правильном порядке:
+import math
+def square(r):
+
+    return math.pi * r ** 2
+radius = input("Введите радиус: ")
+result = square(int(radius))
+print(f"Площадь круга: {result}")
+
+
+
+
