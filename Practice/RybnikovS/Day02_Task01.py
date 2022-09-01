e1732cc61d996612f348e73b215835023902021b
Sergey Rybnikov <kuduzo694@gmail.com>
2022-09-01 22:26:50 +0300

    Правильное окружение пустыми строками функции

--- Practice/RybnikovS/Day02_Task01.py
+++ Practice/RybnikovS/Day02_Task01.py
@@ -3,6 +3,8 @@ import math #добавил 2 пустых строки
 
 def square(r):
     return math.pi * r ** 2
+
+
 radius = input("Введите радиус: ")
 result = square(int(radius))
 print(f"Площадь круга: {result}")
