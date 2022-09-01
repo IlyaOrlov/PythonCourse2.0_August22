42379cf12cbf3bbdd41b02e537767516e20fd64a
Alla <alluska@inbox.ru>
2022-09-01 13:59:52 +0300

    Lec 1 Task 1

---
+++ Practice/aturenko/Homework1.py
@@ -0,0 +1,7 @@
+# Написать эхо-скрипт на Python, запрашивающий любые данные у пользователя и выводящий их с добавлением строки “Echo: ” в начале
+#import Echo as Echo
+
+name = input("Enter your name please: ")
+age = input("Enter you age please: ")
+print(f"\nEcho: {name}")
+print(f"\nEcho: {age}")
