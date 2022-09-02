e96c854c7fef5e52c8bf184b1e1f4cf9116bad12
Dmitry <moisdima@gmail.com>
2022-09-02 18:11:30 +0300

    убрал пустые строки 6 и 8

--- Practice/lesson_2_2.py
+++ Practice/lesson_2_2.py
@@ -3,7 +3,5 @@ end = input("Топлива осталось: ")
 distance = input("Расстояние в км: ")
 
 diff = int(start) - int(end)
-
 result = (diff / int(distance))*100
-
 print(f'Расход топлива на: {distance} км = {result} литров')
