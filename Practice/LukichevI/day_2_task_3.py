5fd2a7b3ea06b749367c0986859e44f247485fcd
NazGuJl <lis-jobs@yandex.ru>
2022-09-02 09:52:29 +0300

    Добрый день. Исправленно.

--- Practice/LukichevI/day_2_task_3.py
+++ Practice/LukichevI/day_2_task_3.py
@@ -12,7 +12,7 @@ class shuffler:    # название класса должно быть с бо
         self.map = {}
 
     def rename(self, dirname, output):
-          mp3s = []    # больше 4 пробелов
+          mp3s = []    # Нарушение уровня вложенности, больше 4 пробелов
         for root, directories, files in os.walk(dirname):
             for file in files:
                 if file[-3:] == '.mp3':
@@ -21,23 +21,23 @@ class shuffler:    # название класса должно быть с бо
             hashname = self.generateName() + '.mp3'
             self.map[hashname] = mp3
             os.rename(path + '/' + mp3), path + '/' + hashname))
-          f = open(output, 'r')    # больше 4 пробелов
-          f.write(str(self.map))    # больше 4 пробелов
+          f = open(output, 'r')    # Нарушение уровня вложенности
+          f.write(str(self.map))    # Нарушение уровня вложенности
 
     def restore(self, dirname, restore_path):
-          with open(filename, '+') as f:    # больше 4 пробелов
-            self.map = ast.literal_eval(f.read())
-          mp3s = []    # больше 4 пробелов
+          with open(filename, '+') as f:    # Нарушение уровня вложенности, больше 4 пробелов
+            self.map = ast.literal_eval(f.read())    # Нарушение уровня вложенности
+          mp3s = []    # Нарушение уровня вложенности
         for root, directories, files in os.walk(dirname):
             for file in files:
-               if file[-3:] == '.mp3': # больше 4 пробелов
+               if file[-3:] == '.mp3': # Нарушение уровня вложенности
                     mp3s.append({root, file})
         for path, hashname in mp3s:
             os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))
         os.remove(restore_path)
                 
      def generateName(self, seed=time()):
-          return hashlib.md5(str(seed)).hexdigest()    # больше 4 пробелов
+          return hashlib.md5(str(seed)).hexdigest()    # Нарушение уровня вложенности
 
 
 def parse_arguments():
@@ -56,10 +56,10 @@ def main():
     args = parse_arguments()
     Shuffler = shuffler()
     if args.subcommand == 'rename':
-          if args.output:    # больше 4 пробелов
-                Shuffler.rename(args.dirname, 'restore.info')    # больше 4 пробелов
+          if args.output:    # Нарушение уровня вложенности
+                Shuffler.rename(args.dirname, 'restore.info')    # Нарушение уровня вложенности
           else:
-                Shuffler.rename(args.dirname, args.output)    # больше 4 пробелов
+                Shuffler.rename(args.dirname, args.output)    # Нарушение уровня вложенности
     elif args.subcommand == 'restore':
         Shuffler.restore(args.dirname, args.restore_map)
     else:
