ba742b7b77576e7f5b7f89f1a577021b823dd522
Chipmunk77 <o.garbis@yandex.ru>
2022-09-03 09:46:59 +0300

    Исправлено в соответствии с требованиями pep8

--- Practice/GarbisO/lec1 t4.py
+++ Practice/GarbisO/lec1 t4.py
@@ -1,4 +1,4 @@
-name=input('Ввведите имя:')
-middlename=input("Введите отчество:")
-residence=input("Введите город проживания: ")
+name = input('Ввведите имя:')
+middlename = input("Введите отчество:")
+residence = input("Введите город проживания: ")
 print(f"Echo: Здравствуйте: {name} {middlename}, Вы из {residence}")
