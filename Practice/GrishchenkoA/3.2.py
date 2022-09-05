170567c93207bec23c1264d5cc90c8bf54ca4e96
AleksandraG88 <alrks_sandra88@mail.ru>
2022-09-05 22:42:22 +0300

    Version 1

---
+++ Practice/GrishchenkoA/3.2.py
@@ -0,0 +1,4 @@
+time=int(input("Введите время, проведенное в пути: "))
+distance=int(input("Введите пройденное расстояние: "))
+speed=distance/time
+print(f"Средняя скорость автомобиля равна: {speed}")
