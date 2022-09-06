3a03d915519c62552ee5cd1802aaaeb756c44a3d
Bogdan <kharin.bogdan-93@yandex.ru>
2022-09-06 15:18:44 +0300

    Добавил lower(), использовал отрицательный индекс
    Signed-off-by: Bogdan <kharin.bogdan-93@yandex.ru>

--- Practice/KharinB/Palindrom.py
+++ Practice/KharinB/Palindrom.py
@@ -1,7 +1,7 @@
-word = input("Введите слово: ")
+word = input("Введите слово: ").lower()
 len_w = len(word)
 for i in range(len_w // 2):
-    if word[i] != word[len_w - (i + 1)]:
+    if word[i] != word[- i - 1]:
         print(False)
         break
 
