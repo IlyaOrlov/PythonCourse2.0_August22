16d1ec361b42696082486db64a6a9add960f4976
Bogdan <kharin.bogdan-93@yandex.ru>
2022-09-06 15:11:57 +0300

    Исправленные замечания pep8.
    Signed-off-by: Bogdan <kharin.bogdan-93@yandex.ru>

--- Practice/KharinB/pep8task.py
+++ Practice/KharinB/pep8task.py
@@ -12,33 +12,33 @@ class shuffler:  # Класс пишется CamelCase
         self.map = {}
 
     def rename(self, dirname, output):
-        mp3s = []
+        mp3s = []  # из-за нарушения вложенности не инициализирована переменная.
 
     for root, directories, files in os.walk(dirname):  # Из-за инициализации переменной mp3s не распознаёт dirname .
         for file in files:
             if file[-3:] == '.mp3':
-                mp3s.append([root, file])  # переменная не иницилиализирована
-    for path, mp3 in mp3s:  # переменная не иницилиализирована
-        hashname = self.generateName() + '.mp3'  # Из-за инициализации переменной mp3s не распознаёт self. Именование переменной должно быть в snake_case (hash_name)
+                mp3s.append([root, file])  # не иницилиализирована переменная mp3s
+    for path, mp3 in mp3s:  # не иницилиализирована переменная mp3s
+        hashname = self.generateName() + '.mp3'  # Из-за инициализации переменной mp3s не распознаёт self.
         self.map[hashname] = mp3
-        os.rename(path + '/' + mp3), path + '/' + hashname))  # Это переменная, закрывающие скобки не нужны
+        os.rename(path + '/' + mp3), path + '/' + hashname))  # Закрывающие скобки не нужны
         f = open(output, 'r')  # Лишние отступы (если файл нужно открыть несколько раз, то наоборот отступов не хватает)
         f.write(str(self.map))  # Из-за инициализации переменной mp3s не распознаёт self.
 
     def restore(self, dirname, restore_path):
         with open(filename, '+') as f:  # Переменная filename не инициализирована. Неверное количество отступов
             self.map = ast.literal_eval(f.read())
-        mp3s = []  # Неверное количество отступов
+        mp3s = []  # из-за нарушения вложенности не инициализирована переменная.
 
     for root, directories, files in os.walk(
             dirname):  # Тут не знаю почему не работает, даже с верным количеством отступов у mp3s не распознаётся.
         for file in files:
             if file[-3:] == '.mp3':
-                mp3s.append({root, file})  # переменная не иницилиализирована
-    for path, hashname in mp3s:  # переменная не иницилиализирована
+                mp3s.append({root, file})  # не иницилиализирована переменная mp3s
+    for path, hashname in mp3s:  # не иницилиализирована переменная mp3s
         os.rename(path + '/' + hashname, path + '/' + self.map[
-            hashname]))  # из-за инициализации mp3s не читает self, лишние закрывающие скобки.
-        os.remove(restore_path)  # неверное количество отступов, должно быть больше.
+            hashname]))  # из-за инициализации mp3s не читает self, лишние две закрывающие скобки.
+        os.remove(restore_path)  # нарушение вложенности.
 
         def generateName(self, seed=time()):
             return hashlib.md5(str(seed)).hexdigest()
