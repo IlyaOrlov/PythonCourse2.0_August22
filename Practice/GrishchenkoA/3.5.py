18e9a0b56ebce747ee751fcb5aa17a3a7ce50b46
AleksandraG88 <alrks_sandra88@mail.ru>
2022-09-08 11:00:30 +0300

    Version 2

--- Practice/GrishchenkoA/3.5.py
+++ Practice/GrishchenkoA/3.5.py
@@ -1,17 +1,8 @@
 import math
 word = input("Введите слово для проверки: ")
 _lower = word.lower()
-word_length = int(len(_lower))
-k = word_length / 2
-if word_length % 2 == 0:
-    z = int(k)
-else:
-    z = k.__ceil__()
-left = _lower[0 : z]
-rights = _lower[-1 : int(k) - 1 : -1]
-if left == rights:
-    print("True")
-else:
-    print("False")
+left = _lower[0 : ]
+rights = _lower[-1 : : -1]
+print(left == rights)
 
 
