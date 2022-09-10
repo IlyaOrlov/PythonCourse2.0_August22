7634a0e8c11689254e05466269953f5c5cd2e279
FliffyKsenia <fliffy_ksenia@mail.ru>
2022-09-10 13:19:38 +0300

    lec 3.5

--- Practice/FliffyKsenia/lec.2.2.py
+++ Practice/FliffyKsenia/lec.2.2.py
@@ -1,5 +1,6 @@
 start = input("Топлива было: ")
 distance = input("Расстояние: ")
 end = input("Топлива осталось: ")
-diff = {int(start) - int(end)}
-print (f"result: {diff / distance}")
+diff = int(start) - int(end)
+print (f"Result: {int (diff) / int (distance)}")
+
