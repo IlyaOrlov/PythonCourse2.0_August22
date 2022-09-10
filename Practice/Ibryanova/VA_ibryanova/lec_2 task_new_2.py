b6ace0aaaad6e6cfe58d363c05124a98f9825a5c
Ibryanova <beva07@mail.ru>
2022-09-10 07:32:35 +0300

    "сделано"

---
+++ Practice/Ibryanova/VA_ibryanova/lec_2 task_new_2.py
@@ -0,0 +1,10 @@
+start = input("Топлива было: ")
+end = input("Топлива осталось: ")
+distance = input("Расстояние: ")
+
+diff = int(start) - int(end)
+result = int(diff) / int(distance)
+sred_result = result * 100
+
+print(f"Автомобиль расходует на 1 км пути {result} литров")
+print(f"Автомобиль расходует на 100 км пути {sred_result} литров")
