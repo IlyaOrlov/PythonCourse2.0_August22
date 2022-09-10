f2a60548b5f6aa2032ab5946dde919edadbc9264
shulgina <natali_shulgina>
2022-09-10 13:20:10 +0300

    Внесены изменения согласно замечаниям

--- Practice/ShulginaN/pep8task.py
+++ Practice/ShulginaN/pep8task.py
@@ -4,45 +4,46 @@ import os
 import hashlib
 import ast
 import argparse
-# возможно, должно быть так: import time
+# должно быть так: import time
 from time import *
-
 
 # имя класса начинается с большой буквы
 class shuffler:
-# магический метод или атрибут с двойным подчеркиванием в начале и в конце
+
     def __init__(self):
         self.map = {}
 
-# неправильный уровень вложенности
     def rename(self, dirname, output):
-          mp3s = []
-        for root, directories, files in os.walk(dirname):
-            for file in files:
-                if file[-3:] == '.mp3':
-                    mp3s.append([root, file])
-        for path, mp3 in mp3s:
-            hashname = self.generateName() + '.mp3'
-            self.map[hashname] = mp3
-            os.rename(path + '/' + mp3), path + '/' + hashname))
-          f = open(output, 'r')
-          f.write(str(self.map))
+        mp3s = []
 
-# неправильный уровень вложенности
+#некорректны уровни вложенности кода внутри функций
+    for root, directories, files in os.walk(dirname):
+        for file in files:
+            if file[-3:] == '.mp3':
+                mp3s.append([root, file])
+    for path, mp3 in mp3s:
+        hashname = self.generateName() + '.mp3'
+        self.map[hashname] = mp3
+        os.rename(path + '/' + mp3), path + '/' + hashname))
+        f = open(output, 'r')
+        f.write(str(self.map))
+
+# некорректны уровни вложенности кода внутри функций
     def restore(self, dirname, restore_path):
-          with open(filename, '+') as f:
+        with open(filename, '+') as f:
             self.map = ast.literal_eval(f.read())
-          mp3s = []
-        for root, directories, files in os.walk(dirname):
-            for file in files:
-               if file[-3:] == '.mp3':
-                    mp3s.append({root, file})
-        for path, hashname in mp3s:
-            os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))
+        mp3s = []
+
+    for root, directories, files in os.walk(dirname):
+        for file in files:
+            if file[-3:] == '.mp3':
+                mp3s.append({root, file})
+    for path, hashname in mp3s:
+        os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))
         os.remove(restore_path)
-                
-     def generateName(self, seed=time()):
-          return hashlib.md5(str(seed)).hexdigest()
+
+    def generateName(self, seed=time()):
+        return hashlib.md5(str(seed)).hexdigest()
 
 
 def parse_arguments():
@@ -57,14 +58,15 @@ def parse_arguments():
     args = parser.parse_args()
     return args
 
+#некорректны уровни вложенности кода внутри функций
 def main():
     args = parse_arguments()
     Shuffler = shuffler()
     if args.subcommand == 'rename':
-          if args.output:
-                Shuffler.rename(args.dirname, 'restore.info')
-          else:
-                Shuffler.rename(args.dirname, args.output)
+        if args.output:
+            Shuffler.rename(args.dirname, 'restore.info')
+        else:
+            Shuffler.rename(args.dirname, args.output)
     elif args.subcommand == 'restore':
         Shuffler.restore(args.dirname, args.restore_map)
     else:
