0b59c753e6748afa54a27f8cb9f10e2980c381b3
Kanateva2(1) <AlexaEltarenko@yandex.ru>
2022-09-10 20:43:47 +0300

    Lek2 Task1

--- Practice/KanatevaA/Лекция 2 Задание№1.py
+++ Practice/KanatevaA/Лекция 2 Задание№1.py
@@ -1,13 +1,7 @@
 import math
 
-
 def sguare(r):
     return math.pi*r**2
-radius=input("введите радиус:")
+radius = input("введите радиус:")
 result = sguare(int(radius))
 print(f"площадь круга:{result}")
-
-
-
-
-
