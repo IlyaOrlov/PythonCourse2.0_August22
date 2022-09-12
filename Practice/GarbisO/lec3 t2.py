4215b874d12d01cb893f957928e1b3a0b75f60a9
Chipmunk77 <o.garbis@yandex.ru>
2022-09-12 11:04:04 +0300

    Лекция 3, задания 1-4

---
+++ Practice/GarbisO/lec3 t2.py
@@ -0,0 +1,4 @@
+distance = float(input("Введите пройденное расстояние в метрах: "))
+time = float(input("Введите затраченное время в секундах: "))
+average_speed = float(distance/time)
+print(f"Средняя скорость движения: {average_speed} м/с")
