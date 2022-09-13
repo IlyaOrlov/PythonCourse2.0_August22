3bfc44b51dd085cf4653c3689b078c38eeb48a43
AleksandraG88 <alrks_sandra88@mail.ru>
2022-09-13 22:34:40 +0300

    Version 3

--- Practice/GrishchenkoA/3.5.py
+++ Practice/GrishchenkoA/3.5.py
@@ -1,8 +1,7 @@
-import math
 word = input("Введите слово для проверки: ")
 _lower = word.lower()
-left = _lower[0 : ]
-rights = _lower[-1 : : -1]
+left = _lower
+rights = _lower[::-1]
 print(left == rights)
 
 
