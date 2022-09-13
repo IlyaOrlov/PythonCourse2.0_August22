789b7d1a8a846401059e19f82a89f1ac9dffb801
Ibryanova <beva07@mail.ru>
2022-09-13 21:36:15 +0300

    home_lec3

---
+++ Practice/Ibryanova/VA_ibryanova/home_lec3_task3.py
@@ -0,0 +1,10 @@
+p = 30265
+k = 666
+res = p ^ k
+print(res)
+
+
+code = 29859
+k = 666
+res = code ^ k
+print(res)
---
+++ Practice/Ibryanova/VA_ibryanova/home_lec3_task5.py
@@ -0,0 +1,7 @@
+s = input("Введите слово: ")
+s = s.lower()
+print(s == s[::-1])
+
+#word = input("Введите слово для проверки палиндрома: ")
+#word = word.lower()
+#print(word == word[::-1])
