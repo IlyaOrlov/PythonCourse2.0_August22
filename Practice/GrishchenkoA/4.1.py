541f31c5d7f1ddc953b71a0e4a61fc5d9c08534d
AleksandraG88 <alrks_sandra88@mail.ru>
2022-09-14 08:46:42 +0300

    Version 2

--- Practice/GrishchenkoA/4.1.py
+++ Practice/GrishchenkoA/4.1.py
@@ -2,13 +2,10 @@ number = 1
 while number <= 100:
     if number % 15 == 0:
         print("FizzBuzz")
-        number += 1
     elif number % 5 == 0:
         print("Buzz")
-        number += 1
     elif number % 3 == 0:
         print("Fizz")
-        number += 1
     else:
         print(number)
-        number += 1
+    number += 1
--- Practice/GrishchenkoA/4.3.py
+++ Practice/GrishchenkoA/4.3.py
@@ -1,12 +1,9 @@
-symbol = input("Введите числовой символ: ")
 numbers=""
-while symbol.lower() != "stop":
+while (symbol := input("Введите числовой символ: ")).lower() != "stop":
     if symbol.isdecimal():
         numbers += symbol
-        symbol = input("Введите числовой символ: ")
     else:
         print("Введен нечисловой символ")
-        symbol = input("Введите числовой символ: ")
 else:
     print(f"Сформированное число: {int(numbers)}")
 
