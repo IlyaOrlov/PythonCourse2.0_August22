e6146684658a1488e1d4fa669c349ee4f35edad4
Alla <alluska@inbox.ru>
2022-09-15 00:21:12 +0300

    Update Lec 3 Task 4, Lec 4 Task 1,4,5,8, Lec 5 Task 6

--- Practice/aturenko/Homework3.py
+++ Practice/aturenko/Homework3.py
@@ -1,40 +1,41 @@
-# # 1.Написать программу для расчёта периметра прямоугольника по введённым длине и ширине.
-#
-# a = float(input("Enter the length of the rectangle: "))
-# b = float(input("Enter the width of the rectangle: "))
-# res = 2 * (a + b)
-# print(f"The perimeter of the rectangle is {res}")
-#
-# # 2.Написать программу для расчёта средней скорости автомобиля по введённым значениям времени и расстояния,
-# # пройденного за это время.
-#
-# t = float(input("Enter the time in hours: "))
-# s = float(input("Enter the distance in kilometers: "))
-# v = s/t
-# print(f"The average speed of the car is {v} kilometers per hour")
-#
-# # 3.Написать две программы: одна должна шифровать введённый пользователем цифровой пароль при помощи секретного кода,
-# # другая – расшифровывать получившийся результат при помощи ЭТОГО ЖЕ секретного кода.
-#
-# password = int(input("Enter 5 integers from 0 to 9: "))
-# key = 12345
-# result = password ^ key
-# print(result)          # Выводит пароль в зашифрованном виде
-# code = result
-# decoding = code ^ key
-# print(decoding)        # Выводит расшифрованный пароль
+# 1.Написать программу для расчёта периметра прямоугольника по введённым длине и ширине.
+
+a = float(input("Enter the length of the rectangle: "))
+b = float(input("Enter the width of the rectangle: "))
+res = 2 * (a + b)
+print(f"The perimeter of the rectangle is {res}")
+
+# 2.Написать программу для расчёта средней скорости автомобиля по введённым значениям времени и расстояния,
+# пройденного за это время.
+
+t = float(input("Enter the time in hours: "))
+s = float(input("Enter the distance in kilometers: "))
+v = s/t
+print(f"The average speed of the car is {v} kilometers per hour")
+
+# 3.Написать две программы: одна должна шифровать введённый пользователем цифровой пароль при помощи секретного кода,
+# другая – расшифровывать получившийся результат при помощи ЭТОГО ЖЕ секретного кода.
+
+password = int(input("Enter 5 integers from 0 to 9: "))
+key = 12345
+result = password ^ key
+print(result)          # Выводит пароль в зашифрованном виде
+code = result
+decoding = code ^ key
+print(decoding)        # Выводит расшифрованный пароль
 
 # 4.Написать программу, заменяющую все буквы “А” в слове, введённом пользователем, на символ “*”.
 
 word = input("Enter one word: ")
 
 if "a" in word:
-    word2 = word.replace("a","*")
+    word = word.replace("a","*")
+    print(word)
 else:
     print("There is no letter 'a' in this word")
 if "A" in word:
-    word3 = word2.replace("A", "*")
-    print(word3)
+    word = word.replace("A", "*")
+    print(word)
 else:
     print("There is no letter 'A' in this word")
 
@@ -44,10 +45,10 @@ else:
 # 5. Написать программу, проверяющую, что слово, введённое пользователем,
 # является палиндромом (примеры: “Топот”, “Довод”). Программа должна выводить True или False
 
-# pal = input("Enter one word: ")
-# pal1 = pal.lower()
-#
-# print(pal1 == pal1[::-1])
+pal = input("Enter one word: ")
+pal1 = pal.lower()
+
+print(pal1 == pal1[::-1])
 
 
 
