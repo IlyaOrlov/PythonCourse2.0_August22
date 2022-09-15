c8877d56a447bf5dd219e8a6971f23f5759cb4c7
AleksandraG88 <alrks_sandra88@mail.ru>
2022-09-15 10:03:42 +0300

    Version 1

---
+++ Practice/GrishchenkoA/4.6.py
@@ -0,0 +1,19 @@
+def choose_max(a, b):
+    if a > b:
+        max_1 = a
+    else:
+        max_1 = b
+    print(f"Большее число из введенных: {max_1}")
+
+
+def finds_max(a, b):
+    if a > b:
+        max_2 = a
+    else:
+        max_2 = b
+    return max_2
+
+
+a, b=map(int, input("Введите два числа: ").split())
+choose_max(a, b)
+finds_max(a, b)
