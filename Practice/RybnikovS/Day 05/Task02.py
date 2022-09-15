2b28a6af9337eea9e0bf13860682e64ccfe8cf0d
Sergey Rybnikov <kuduzo694@gmail.com>
2022-09-16 00:29:07 +0300

    Задания 5 лекции. 1 и 2

---
+++ Practice/RybnikovS/Day 05/Task02.py
@@ -0,0 +1,33 @@
+old_arr = [2, 3, 4, 5, 3, 2]
+
+#Время выполнения O(n**2)
+# min_index = len(arr)
+# for i in range(len(arr)):
+#     for j in range(i+1, len(arr)):
+#         if arr[i] == arr[j] and j < min_index:
+#             min_index = j
+#             break
+# print(arr[min_index]) O(n**2)
+
+#Время выполнения O(n*log(n)) - лучшее
+arr_second = []
+arr = list(sorted(enumerate(old_arr), key=lambda x: x[1]))  # O(n*log(n)) > O(n) => O(n*log(n))
+print(arr)
+value_to_ignore = None
+for i in range(len(arr)-1):  # O(n)
+    if value_to_ignore == arr[i][1]:
+        continue
+    if arr[i][1] == arr[i+1][1]:
+        arr_second.append(arr[i+1][0])
+        value_to_ignore = arr[i][1]
+print(old_arr[min(arr_second)])
+
+
+
+
+
+
+
+
+#2, 3, 4, 5, 3, 2
+#2  2  3  3  4  5
