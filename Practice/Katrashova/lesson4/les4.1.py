2002854ffd53913bba8843a30a2a32cf9e201c93
Ksenia <yurchenkova.k@mail.ru>
2022-09-18 16:48:28 +0300

    Урок 4,задание 1

--- Practice/Katrashova/lesson4/les4.1.py
+++ Practice/Katrashova/lesson4/les4.1.py
@@ -1,12 +1,14 @@
 from random import random, randint
 
-a = randint (1, 100)
-
-if a % 3 == 0:
-    print("Fizz")
-elif a % 5 == 0:
-    print("Buzz")
-elif a % 15 == 0:
-    print("FizzBuzz")
-else:
-    print(a)
+a = 0
+while a<=99:
+    a = a+1
+    if a % 15 == 0:
+        print("FizzBuzz")
+    elif a % 5 == 0:
+        print("Buzz")
+    elif a % 3 == 0:
+        print("Fizz")
+    else:
+        print(a)
+        continue
