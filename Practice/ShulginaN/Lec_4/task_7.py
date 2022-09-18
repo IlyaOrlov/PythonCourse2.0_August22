7b3f9dd27608c81e1da136c3437875646c2f34f1
shulgina <natali_shulgina>
2022-09-18 15:34:14 +0300

    Иль, объясните пож-та как сделть "===" после вывода 200?

--- Practice/ShulginaN/Lec_4/task_7.py
+++ Practice/ShulginaN/Lec_4/task_7.py
@@ -1,33 +1,44 @@
 def decorator(fun):
-    def in_decorator(x, y):
+    def in_decorator(*args, **kwargs):
         print("===========")
-        fun(x, y)
+        res = fun(*args, **kwargs)
         print("===========")
+        return res
 
     return in_decorator
 
 
-# 1й способ
 @decorator
-def max_number(a, b):
-    if a > b:
-        print(a)
-    else:
-        print(b)
+def my_example(x):
+    return x * x
 
-def max_number2(a, b):
-    if a > b:
-        print(a)
-    else:
-        print(b)
+res = my_example(10) * 2
+print(res)
 
-x = input("Введите 'a': ")
-y = input("Введите 'b': ")
-max_number(x, y)
 
-# 2й способ
-max_number2 = decorator(max_number2)
 
-n = input("Введите 'a': ")
-m = input("Введите 'b': ")
-max_number2(n, m)
+#
+# # 1й способ
+# @decorator
+# def max_number(a, b):
+#     if a > b:
+#         print(a)
+#     else:
+#         print(b)
+#
+# def max_number2(a, b):
+#     if a > b:
+#         print(a)
+#     else:
+#         print(b)
+#
+# x = input("Введите 'a': ")
+# y = input("Введите 'b': ")
+# max_number(x, y)
+#
+# # 2й способ
+# max_number2 = decorator(max_number2)
+#
+# n = input("Введите 'a': ")
+# m = input("Введите 'b': ")
+# max_number2(n, m)
