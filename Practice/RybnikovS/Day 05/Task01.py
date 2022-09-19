a584420c27c0a07676b6998a80aa9d73d28a0d15
Sergey Rybnikov <kuduzo694@gmail.com>
2022-09-19 19:32:06 +0300

    Task 01 - добавлена проверка на индекс Task 02 - реализовано через словарь и set

--- Practice/RybnikovS/Day 05/Task01.py
+++ Practice/RybnikovS/Day 05/Task01.py
@@ -9,5 +9,6 @@ def find_min_in_arr(array):
 arr = [0, 3, 24, 2, 3, 7]
 for i in range(len(arr)-1):
     min_index = find_min_in_arr(arr[i:])
-    arr[i], arr[i+min_index] = arr[i+min_index], arr[i]
+    if i != min_index:
+        arr[i], arr[i+min_index] = arr[i+min_index], arr[i]
 print(arr)
--- Practice/RybnikovS/Day 05/Task02.py
+++ Practice/RybnikovS/Day 05/Task02.py
@@ -10,14 +10,37 @@ old_arr = [2, 3, 4, 5, 3, 2]
 # print(arr[min_index])
 
 #Время выполнения O(n*log(n)) - лучшее
-arr_second = []
-arr = list(sorted(enumerate(old_arr), key=lambda x: x[1]))  # O(n*log(n)) > O(n) => O(n*log(n))
-print(arr)
-value_to_ignore = None
-for i in range(len(arr)-1):  # O(n)
-    if value_to_ignore == arr[i][1]:
-        continue
-    if arr[i][1] == arr[i+1][1]:
-        arr_second.append(arr[i+1][0])
-        value_to_ignore = arr[i][1]
-print(old_arr[min(arr_second)])
+# arr_second = []
+# arr = list(sorted(enumerate(old_arr), key=lambda x: x[1]))  # O(n*log(n)) > O(n) => O(n*log(n))
+# print(arr)
+# value_to_ignore = None
+# for i in range(len(arr)-1):  # O(n)
+#     if value_to_ignore == arr[i][1]:
+#         continue
+#     if arr[i][1] == arr[i+1][1]:
+#         arr_second.append(arr[i+1][0])
+#         value_to_ignore = arr[i][1]
+# print(old_arr[min(arr_second)])
+
+
+#Время выполнения O(n) через словарь - лучше лучшего!
+#
+# print(old_arr)
+# dict = {}
+# for i in range(len(old_arr)):  # O(n)
+#     if dict.get(old_arr[i]) is None:  # O(1)
+#         dict[old_arr[i]] = 1  # O(1)
+#     else:
+#         print(f"Первый повторившийся элемент {old_arr[i]}")
+#         break
+
+#Время выполнения O(n) через set - лучше лучшего!
+
+print(old_arr)
+dict = set()
+for i in range(len(old_arr)):  # O(n)
+    if old_arr[i] in dict:   # O(1)
+        print(f"Первый повторившийся элемент {old_arr[i]}")   # O(1)
+        break
+    else:
+        dict.add(old_arr[i])  # O(1)
