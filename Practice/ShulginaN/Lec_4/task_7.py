4a751e9438695c3f66688151dc5259f6220af67e
shulgina <natali_shulgina>
2022-09-19 18:11:19 +0300

    Убрала закоментированные строки

--- Practice/ShulginaN/Lec_4/task_7.py
+++ Practice/ShulginaN/Lec_4/task_7.py
@@ -2,7 +2,6 @@ def decorator(fun):
     def in_decorator(*args, **kwargs):
         print("===========")
         res = fun(*args, **kwargs)
-        print("===========")
         return res
 
     return in_decorator
@@ -15,30 +14,3 @@ def my_example(x):
 res = my_example(10) * 2
 print(res)
 
-
-
-#
-# # 1й способ
-# @decorator
-# def max_number(a, b):
-#     if a > b:
-#         print(a)
-#     else:
-#         print(b)
-#
-# def max_number2(a, b):
-#     if a > b:
-#         print(a)
-#     else:
-#         print(b)
-#
-# x = input("Введите 'a': ")
-# y = input("Введите 'b': ")
-# max_number(x, y)
-#
-# # 2й способ
-# max_number2 = decorator(max_number2)
-#
-# n = input("Введите 'a': ")
-# m = input("Введите 'b': ")
-# max_number2(n, m)
