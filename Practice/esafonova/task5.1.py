a668562a1582c2543bdf9bb88892c84d9442839a
SafonovaEkaterina <doinikova-ekaterina@yandex.ru>
2022-09-19 16:55:53 +0300

    update files 5.1

---
+++ Practice/esafonova/task5.1.py
@@ -0,0 +1,12 @@
+def sort(arr):
+    for i in range(len(arr)):
+        m = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[m]:
+                m = j
+        arr[m],arr[i] = arr[i], arr[m]
+    return arr
+
+arr = [6,3,24,2,3,7]
+res = sort(arr)
+print(res)
