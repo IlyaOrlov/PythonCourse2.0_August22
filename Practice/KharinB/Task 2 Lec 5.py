40317c9631eea3f3e5a76d7b8ce8471138be0be2
Bogdan <kharin.bogdan-93@yandex.ru>
2022-09-20 19:50:06 +0300

    Задача 4 лекция 5
    Signed-off-by: Bogdan <kharin.bogdan-93@yandex.ru>

--- Practice/KharinB/Task 2 Lec 5.py
+++ Practice/KharinB/Task 2 Lec 5.py
@@ -1,5 +1,5 @@
 arr = input("Введите массив:", ) or [2, 3, 4, 5, 3, 2]
 for i in range(len(arr)):
-    if arr[i::][0] in arr[:i:]:
+    if arr[i] in arr[:i:]:  # заменил на arr[i]
         print(arr[i])
         break
