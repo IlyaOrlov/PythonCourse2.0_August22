10f08ee79c02455d8909fbd21867249f747797c3
SafonovaEkaterina <doinikova-ekaterina@yandex.ru>
2022-09-20 11:47:25 +0300

    update files 5.1 add if m!=i

--- Practice/esafonova/task5.1.py
+++ Practice/esafonova/task5.1.py
@@ -4,9 +4,10 @@ def sort(arr):
         for j in range(i + 1, len(arr)):
             if arr[j] < arr[m]:
                 m = j
-        arr[m], arr[i] = arr[i], arr[m]
+        if m != i:
+            arr[m], arr[i] = arr[i], arr[m]
     return arr
 
-arr = [6,3,24,2,3,7]
+arr = [6,3,24,26,3,7]
 res = sort(arr)
 print(res)
--- Practice/esafonova/task5.2.py
+++ Practice/esafonova/task5.2.py
@@ -3,6 +3,6 @@ def rep(arr):
         if arr[i] in arr[:i:]:
             return arr[i]
 
-arr = [2, 3, 2, 5, 3, 2]
+arr = [2, 3, 7, 5, 3, 2]
 res = rep(arr)
 print(res)
