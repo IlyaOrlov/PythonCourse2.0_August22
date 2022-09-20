55174def9c042876db5e39fb2f317ddedce7bc13
NazGuJl <lis-jobs@yandex.ru>
2022-09-20 15:30:33 +0300

    Нашел ошибку

--- Practice/LukichevI/Lec_5/Lec_5_task_2.py
+++ Practice/LukichevI/Lec_5/Lec_5_task_2.py
@@ -3,7 +3,7 @@ def fun():
         if (lst1[i] in lst2) == False:
             lst2.append(lst1[i])
         else:
-            print(f'Первый повторившийся символ: {lst[i]} ')
+            print(f'Первый повторившийся символ: {lst1[i]} ')
 
 
 lst1 = [2, 3, 4, 5, 3, 2]
