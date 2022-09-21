1488e614da43bc22eaa4f606c727136791d9c2ad
AleksandraG88 <alrks_sandra88@mail.ru>
2022-09-21 11:03:09 +0300

    Version 1

---
+++ Practice/GrishchenkoA/5.4.py
@@ -0,0 +1,16 @@
+def delete_column(lst, number):
+    for i, d in enumerate(lst):
+        for y, v in enumerate(d):
+            if number == int(v):
+                for k in range(len(lst_1)):
+                    del lst_1[k][y]
+
+
+lst_1 = [[1, 2, 3, 4], [7, 8, 9, 5], [5, 6, 7, 4], [1, 2, 9, 5]]
+number_1 = int(input("Введите цифру: "))
+delete_column(lst_1, number_1)
+print(lst_1)
+
+
+
+
