36c889866b151c266af518c7f7f64667b1323ab3
Bogdan <kharin.bogdan-93@yandex.ru>
2022-09-21 16:57:34 +0300

    Задача 1 лекция 5
    Signed-off-by: Bogdan <kharin.bogdan-93@yandex.ru>

--- Practice/KharinB/Task 1 Lec 5.py
+++ Practice/KharinB/Task 1 Lec 5.py
@@ -1,9 +1,6 @@
 arr = [0, 3, 0, 24, 2, 3, 8, 6, 8, 78, 8, 7]
-arr = list(enumerate(arr, 0))
 for i in range(len(arr)):
-    x = arr[i::]
-    y = min(x, key=lambda i: i[1])[0]
-    arr[y], arr[i] = (arr[y][0], arr[i][1]), arr[y][1]
+    for j in range(i, len(arr)):
+        if arr[i] > arr[j]:
+            arr[i], arr[j] = arr[j], arr[i]
 print(arr)
-
-# Я конечно решил задачу 4.2 не так, но намёк понял)))
