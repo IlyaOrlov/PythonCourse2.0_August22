de8f28c18276ad9a1604c755d289edf09c0c70ad
NazGuJl <lis-jobs@yandex.ru>
2022-09-22 11:46:55 +0300

    Внес корректировки, в соответствии с замечаниями.

--- Practice/LukichevI/Lec_5/Lec_5_task_1.py
+++ Practice/LukichevI/Lec_5/Lec_5_task_1.py
@@ -1,13 +1,15 @@
-def sort_lst(arr):
-    for i in range(len(arr) - 1):
-        min = i
-        for a in range(i + 1, len(arr)):
-            if arr[a] < arr[min]:
-                min = a
-        arr[i], arr[min] = arr[min], arr[i]
+def sort_lst(b):
+    for i in range(len(b)):
+        minimum = i
+        for a in range(i + 1, len(b)):
+            if b[a] < b[minimum]:
+                minimum = a
+        if i != minimum:
+            b[i], b[minimum] = b[minimum], b[i]
 
 
 arr = [0,3,24,2,3,7]
 sort_lst(arr)
 print(f'Список отсортирован: {arr}')
+
 
--- Practice/LukichevI/Lec_5/Lec_5_task_2.py
+++ Practice/LukichevI/Lec_5/Lec_5_task_2.py
@@ -1,11 +1,11 @@
-def fun():
+def fun(a, b):
     for i in range(len(lst1) - 1):
-        if (lst1[i] in lst2) == False:
-            lst2.append(lst1[i])
-        else:
+        if a[i] in b:
             print(f'Первый повторившийся символ: {lst1[i]} ')
+        else:
+            b.add(a[i])
 
 
 lst1 = [2, 3, 4, 5, 3, 2]
-lst2 = []
-fun()
+lst2 = set()
+fun(lst1, lst2)
