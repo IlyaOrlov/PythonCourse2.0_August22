47f66ba7003dd672b1ea11b89c44201848fb0282
Ksenia <yurchenkova.k@mail.ru>
2022-09-22 13:02:42 +0300

    исправила

--- Practice/Katrashova/lesson4/les4.2.py
+++ Practice/Katrashova/lesson4/les4.2.py
@@ -1,5 +1,8 @@
-a = input("Введите пятизначное число: ")
-i = 1
-while i in range(len[a]):
-    print("{i} цифра равна (a{i})")
-    i = i+1
+while True:
+    n = input("Введите число: ")
+    if not n.isdigit():
+        print("Ошибка введите ещё раз")
+    else:
+        break
+for i in range(len(n)):
+    print(f"{i+1} число равно {n[i]}")
