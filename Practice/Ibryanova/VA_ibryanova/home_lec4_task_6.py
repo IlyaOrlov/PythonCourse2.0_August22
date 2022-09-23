b7e3477a0732229d71f244e34097c0ad5e915e28
Ibryanova <beva07@mail.ru>
2022-09-23 05:40:35 +0300

    home_lec4_task6.py

--- Practice/Ibryanova/VA_ibryanova/home_lec4_task_6.py
+++ Practice/Ibryanova/VA_ibryanova/home_lec4_task_6.py
@@ -12,3 +12,14 @@ print(res)
 #y = int(input('Введите целое число y: '))
 #f = lambda a, b: a if a > b else b
 #print(f(x, y))
+
+def max_num2(a, b):
+    if a > b:
+        print(a)
+    else:
+        print(b)
+
+a = int(input('Введите целое число x: '))
+b = int(input('Введите целое число y: '))
+res = max_num2(a, b)
+print(res)
