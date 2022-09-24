fdf5b7fc29df46f5b2bac23dbffac03436baff43
NazGuJl <lis-jobs@yandex.ru>
2022-09-24 15:55:30 +0300

    Исправлено)

--- Practice/LukichevI/Lec_5/Lec_5_task_2.py
+++ Practice/LukichevI/Lec_5/Lec_5_task_2.py
@@ -1,11 +1,11 @@
 def fun(my_list):
-    new_list = set()
+    new_set = set()
     for i in range(len(my_list)):
-        if my_list[i] in new_list:
+        if my_list[i] in new_set:
             print(f'Первый повторившийся символ: {my_list[i]} ')
             break
         else:
-            new_list.add(my_list[i])
+            new_set.add(my_list[i])
 
 
 lst = [2, 3, 4, 5, 3, 2]
