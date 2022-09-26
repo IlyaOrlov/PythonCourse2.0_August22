d6b159fecf90ba03492e891670c9fa0c0f80cae0
AleksandraG88 <alrks_sandra88@mail.ru>
2022-09-26 09:22:12 +0300

    Исправила, теперь правильно удаляет

--- Practice/GrishchenkoA/5.4.py
+++ Practice/GrishchenkoA/5.4.py
@@ -2,15 +2,18 @@ def delete_column(lst, number):
     for i, d in enumerate(lst):
         for y, v in enumerate(d):
             if number == int(v):
-                for k in range(len(lst_1)):
+                for k in range(len(lst)):
+                    del lst[k][y]
+        for y, v in enumerate(d):
+            if number == int(v):
+                for k in range(len(lst)):
                     del lst_1[k][y]
 
 
-lst_1 = [[1, 2, 3, 4], [7, 8, 9, 5], [5, 6, 7, 4], [1, 2, 9, 5]]
+lst_1 = [[1, 2, 2, 4], [7, 8, 9, 5], [5, 6, 7, 4], [1, 2, 9, 5]]
 number_1 = int(input("Введите цифру: "))
 delete_column(lst_1, number_1)
 print(lst_1)
-
 
 
 
