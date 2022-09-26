e363ea17e8a857b9faf3c5484f4e4311e9f91dc8
AleksandraG88 <alrks_sandra88@mail.ru>
2022-09-26 10:08:50 +0300

    Поправила

--- Practice/GrishchenkoA/4.5.py
+++ Practice/GrishchenkoA/4.5.py
@@ -3,21 +3,21 @@ import random
 range = input("Введите диапазон чисел: ").split()
 hidden_numbers = random.randint(int(range[0]), int(range[1]))
 numbers_1 = input("Угадайте целое число: ")
-if numbers_1.isdigit():
-    while int(numbers_1) != hidden_numbers:
-        if not numbers_1.isdigit():
-            print("Вы ввели нечисловой символ")
-            break
+while True:
+    if not numbers_1.isdigit():
+        print("Вы ввели нечисловой символ")
+        break
+    else:
+        numbers_1 = int(numbers_1)
+        if numbers_1 < hidden_numbers:
+            print("Загаданное число больше введенного")
+        elif numbers_1 > hidden_numbers:
+            print("Загаданное число меньше введенного")
         else:
-            numbers_1 = int(numbers_1)
-            if numbers_1 < hidden_numbers:
-                print("Загаданное число больше введенного")
-            elif numbers_1 > hidden_numbers:
-                print("Загаданное число меньше введенного")
-        numbers_1 = input("Угадайте целое число: ")
-    print("Вы угадали")
-else:
-     print("Вы ввели нечисловой символ")
+            print("Вы угадали")
+            break
+    numbers_1 = input("Угадайте целое число: ")
+
 
 
 
--- Practice/GrishchenkoA/5.1.py
+++ Practice/GrishchenkoA/5.1.py
@@ -1,11 +1,21 @@
 def find_min_index(lst):
-    a = min(lst[i:])
-    c = lst.index(a, i)
-    lst[c], lst[i] = lst[i], lst[c]
+    for i,d in enumerate(lst):
+        d = min(lst[i:])
+        d, lst[i] = lst[i], d
 
 
 arr = [0, 3, 24, 2, 3, 7]
-for i in range(0, len(arr)):
-    find_min_index(arr)
+#for i in range(0, len(arr)):
+find_min_index(arr)
 print(arr)
 
+#def find_min_index(lst):
+  #  a = min(lst[i:])
+   # c = lst.index(a, i)
+    #lst[c], lst[i] = lst[i], lst[c]
+
+
+#arr = [0, 3, 24, 2, 3, 7]
+#for i in range(0, len(arr)):
+   # find_min_index(arr)
+#print(arr)
