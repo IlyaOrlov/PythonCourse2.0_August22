61c5ccf07a2b6f81167cb9dd088150364c2749df
AleksandraG88 <alrks_sandra88@mail.ru>
2022-09-26 09:42:32 +0300

    Поправила

--- Practice/GrishchenkoA/5.3.py
+++ Practice/GrishchenkoA/5.3.py
@@ -1,11 +1,11 @@
-str = "Петров - 2 , Иванов - 5 , Сидоров - 4"
-lst = list(str.split())
+str_new = "Петров - 2 , Иванов - 5 , Сидоров - 4"
+lst = list(str_new.split())
 dict_1 = {2: "неудовлетворительно", 3: "удовлетворительно", 4: "хорошо", 5: "отлично"}
 for y, b in enumerate(lst):
-    for i, d in enumerate(dict_1):
-        if b.isdigit():
-            if int(b) == d:
-                lst[y] = dict_1[d]
+    if b.isdigit():
+        b = int(b)
+        if b in dict_1:
+            lst[y] = dict_1[b]
 str_1 = ""
 for i in lst:
     print(str_1.join(i), end=" ")
