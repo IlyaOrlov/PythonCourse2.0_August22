083f7c07255a2ad940c2e3388f5cd2e6273816ae
Bogdan <kharin.bogdan-93@yandex.ru>
2022-09-27 13:13:42 +0300

    Задача 5 лекция 5
    Signed-off-by: Bogdan <kharin.bogdan-93@yandex.ru>

---
+++ Practice/KharinB/Task 5 Lec 5.py
@@ -0,0 +1,20 @@
+def tab_space(p, tab_space="t"):
+    try:
+        f = open(p, "r+")
+    except:
+        print("Файл не найден")
+    else:
+        text = f.read()
+        if tab_space == "s":
+            text = text.replace("    ", " ")
+        elif tab_space == "t":
+            text = text.replace(" ", "    ")
+        else:
+            print("Неверно указан тип изменения")
+            return None
+        f.seek(0)
+        f.write(text)
+        f.close()
+
+
+tab_space("text.txt")
