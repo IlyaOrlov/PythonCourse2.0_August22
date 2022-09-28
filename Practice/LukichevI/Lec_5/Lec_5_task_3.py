268663868e633b07bdbfd7c974148b08baa08683
NazGuJl <lis-jobs@yandex.ru>
2022-09-28 14:21:34 +0300

    Внес изменения в задачи 3 b 4 в соответствии с замечаниями.

--- Practice/LukichevI/Lec_5/Lec_5_task_3.py
+++ Practice/LukichevI/Lec_5/Lec_5_task_3.py
@@ -2,9 +2,7 @@ my_string = 'Иван родил дитенка велел тащить пеле
 my_string = my_string.split()
 my_dict = {'родил': 'купил', 'дитенка': 'самогонки', 'пеленки': 'закуски'}
 for i in range(len(my_string)):
-    word = my_string[i]
-    for a in my_dict:
-        if a == word:
-            my_string[i] = my_dict[a]
+    if my_string[i] in my_dict:
+        my_string[i] = my_dict[my_string[i]]
 my_string = ' '.join(my_string)
 print(my_string)
--- Practice/LukichevI/Lec_5/Lec_5_task_4.py
+++ Practice/LukichevI/Lec_5/Lec_5_task_4.py
@@ -1,20 +1,21 @@
 def count(my_lst, x):
-    idx_lst = []
-    for i in range(len(my_lst) - 1, -1, -1):
-        for a in range(len(my_lst) - 1, -1, -1):
+    idx_lst = set()
+    for i in range(len(my_lst)):
+        for a in range(len(my_lst)):
             if x == my_lst[i][a]:
-                idx_lst.append(a)
+                idx_lst.add(a)
+    idx_lst = list(idx_lst)
     return idx_lst
 
 
 def del_num(my_lst, idx_lst):
     for i in range(len(my_lst)):
-        for a in idx_lst:
+        for a in reversed(idx_lst):
             del my_lst[i][a]
     return my_lst
 
 
-lst = [[1, 2, 2], [4, 5, 6], [7, 8, 9]]
+lst = [[1, 2, 2], [4, 5, 2], [7, 8, 9]]
 #       0  1  2    0  1  2    0  1  2
 #          0          1          2
 a = int(input('Введите счисло на удаление от 1 до 9: '))
