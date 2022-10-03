6c3dcc2f807153faddfdda7c5f9982d0e92e22ff
Bogdan <kharin.bogdan-93@yandex.ru>
2022-10-04 00:02:00 +0300

    Задание 1 лекция 8

---
+++ Practice/KharinB/Task 1 Lec 8.py
@@ -0,0 +1,31 @@
+class MyIterTxt:
+
+    def __init__(self, f, sim):
+        self._file_path = f
+        self._sim = sim
+        self._ind = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        with open(self._file_path, "r") as f:
+            f.seek(self._ind)
+            res = f.read(1)
+            if res != "":
+                while True:
+                    s = f.read(1)
+                    if s != self._sim and s != "":
+                        res += s
+
+                    else:
+                        self._ind = f.tell()
+                        return res
+
+            else:
+                raise StopIteration
+
+
+for i in MyIterTxt("text.txt", "@"):
+    print("________________")
+    print(i)
