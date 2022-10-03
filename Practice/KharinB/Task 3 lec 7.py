ae26cbff8422d694c1bb218411ce3afcb2f20692
Bogdan <kharin.bogdan-93@yandex.ru>
2022-10-03 22:36:05 +0300

    Исправлено

--- Practice/KharinB/Task 3 lec 7.py
+++ Practice/KharinB/Task 3 lec 7.py
@@ -1,12 +1,9 @@
-class Terminal:
+class PerTer:
     _account = [0]
-    _cash = "Терминал не хранит наличные"
 
-    def __init__(self, name):
-        self._name = name
-
-    def check_acc(self):
-        print(f"На вашем счету: {self._account[0]}")
+    def __init__(self, name, cash):
+        self.name = name
+        self._cash = cash
 
     @property
     def cash(self):
@@ -21,12 +18,28 @@ class Terminal:
         else:
             print("Введео некорректное число")
 
+    def show_info(self):
+        met = []
+        for j in self.__dir__():
+            if j[0] != "_" and j != 'show_info':
+                met.append(j)
+        print("------------")
+        print(f"наличние в терминале {self.name}:{self._cash}")
+        print(met)
 
-class Banckomat(Terminal):
 
+class Terminal(PerTer):
+    def __init__(self, name):
+        super().__init__(name, "Терминал не хранит наличные")
+
+
+    def check_acc(self):
+        print(f"На вашем счету: {self._account[0]}")
+
+
+class Banckomat(PerTer):
     def __init__(self, name, cash):
-        super().__init__(self, name)
-        self._cash = cash
+        super().__init__(name, cash)
 
     def replenish(self):
         a = input("Вставьте купюры в купюроприёмник")
@@ -53,8 +66,9 @@ class Banckomat(Terminal):
                 print(f"На Вашем счету осталось: {self._account[0]}")
 
 
-class Acquiring(Terminal):
-    _cash = 0
+class Acquiring(PerTer):
+    def __init__(self, name):
+        super().__init__(name, 0)
 
     def pay(self):
         a = input("Введите сумму покупки")
@@ -71,10 +85,6 @@ class Acquiring(Terminal):
                 print("Ваш чек\n")
                 print("Благодарим за покупку\n")
 
-    @staticmethod
-    def check_acc():
-        print("Терминал экваринга не располагает информацией о Вашем счёте. Как Вы вообще умудрились вызвать это сообщение?!")
-
 
 b1 = Terminal("Terminal")
 b2 = Banckomat("Banckomat", 100000)
@@ -82,18 +92,17 @@ b3 = Acquiring("Acquiring")
 
 
 b2.replenish()
+print()
 b2.withdrawal()
+print()
 b1.check_acc()
+print()
 b3.pay()
+print()
 b1.check_acc()
-print("==============")
+
 
 
 lst = [b1, b2, b3]
 for i in lst:
-    met = []
-    for j in dir(i):
-        if j[0] != "_":
-            met.append(j)
-    print(i.cash)
-    print(met)
+    i.show_info()
