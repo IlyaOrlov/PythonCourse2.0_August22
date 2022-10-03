3e9041adc7a7a50a1e7255f819a89b43bf40f37d
shulgina <natali_shulgina>
2022-10-03 22:08:55 +0300

    Последняя, точнее первая

--- Practice/ShulginaN/Lec_5/task_1.py
+++ Practice/ShulginaN/Lec_5/task_1.py
@@ -1,8 +1,10 @@
 arr = [0, 3, 24, 2, 3, 7]
 
 for i in range(len(arr)):
-    for m in range(i, len(arr)):
-        if arr[i] > arr[m]:
-            arr[i], arr[m] = arr[m], arr[i]
+    m = i
+    for a in range(i + 1, len(arr)):
+        if arr[a] < arr[m]:
+            m = a
+    if i != m:
+        arr[i], arr[m] = arr[m], arr[i]
 print(arr)
-
