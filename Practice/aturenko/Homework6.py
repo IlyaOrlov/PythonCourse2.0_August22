b45ebb0de565c1c55ce66caa121894bad8d4a2cb
Alla <alluska@inbox.ru>
2022-10-04 16:40:16 +0300

    Update Lec5 Task 7, Lec 6 Task 1,2,3,4,5

--- Practice/aturenko/Homework6.py
+++ Practice/aturenko/Homework6.py
@@ -10,11 +10,12 @@ def mymul(x, y):
 
 
 def decorator_fun(func):
-    def wrapper(x, y):
+    def wrapper(*args, **kwargs):
         print("===========")
-        res = func(x, y)
+        res = func(*args, **kwargs)
         print(res)
         print("===========")
+        return res
     return wrapper
 
 
@@ -36,42 +37,105 @@ mymul(10, 20)
 #
 # // на выходе должен получиться список, содержащий [0, 2, 3, 3, 7, 24]
 
-arr = [0,3,24,2,3,7]
-newarr = []
-while len(arr) > 0:
-    newarr.append(min(arr))
-    arr.remove(min(arr))
-print(newarr)
+# arr = [0,3,24,2,3,7]
+# newarr = []
+# while len(arr) > 0:
+#     newarr.append(min(arr))
+#     arr.remove(min(arr))
+# print(newarr)
+
+def sort(arr):
+    for i in range(len(arr)):
+        m = i # индекс элемента
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[m]:
+                m = j
+        if m != i:
+            arr[m], arr[i] = arr[i], arr[m]
+    return arr
+
+
+arr = [4,3,24,2,3,7]
+res = sort(arr)
+print(res)
+
 
 # 2.Написать и вызвать функцию, возвращающую первый повторившийся символ в переданном списке.
 # Например, для  списка [2, 3, 4, 5, 3, 2] функция должна вернуть 3.
 
 
-
-a = []
-for element in input("Введите числа через пробел: ").split():
-    a.append(int(element))
-print(a)
-# b = a
-c=[]
+def retrep(arr):
+    for i in arr:
+        if arr[i] in arr[:i:]:
+            return arr[i]
 
 
-def retrep(a):
-    for i in a:
-        i = 0
-        while i <= len(a):
-            for a[i] in a[i+1:len(a):]: # хочется найти а[i] в срезе типа дальше него самого по порядку..
-                                        # но то что я написала делает не то, а как правильно написать
-                                        # я что-то не могу понять.Запуталась совсем.
+arr = [2, 3, 4, 5, 3, 2]
+res = retrep(arr)
+print(res)
 
-                c.append(i)             # здесь я хочу добавлять индексы
-                                        # найденных в срезе одинаковых с изначальным чисел в список "c",
-                                        # чтобы потом найти наименьший элемент(индекс повторяющегося вновь числа)
-                                        # в списке С
-                i = i + 1
-                                        # Тут я уже остановилась, потомучто понимаю,
-                                        # что не сравниваю в итоге числа с числами из последующего среза..
-                                        # и не знаю как это сделать.
+# 3.Найти и заменить некие шаблоны в строке: есть строка с определенного вида форматированием,
+# необходимо заменить в этой строке все вхождения шаблонов на их значение из словаря.
+
+d = {"туда":"в даль", "ведет":"идет", "длинная":"продолжительная","тропа":"дорога"}
+s = "туда ведет длинная тропа..."
+
+print(f'Первоначальная строка: \n{s}')
+for key in d:
+    s = s.replace(str(key), str(d[key]))
+print(f'Изменённая строка: \n{s}')
+
+# 4.Есть список списков (матрица). Каждый внутренний список – это строка матрицы.
+# Необходимо реализовать функцию, которая удаляет столбец, который содержит заданную цифру.
 
 
-print(retrep([2, 3, 4, 5, 3, 2]))
+def delcolumn(anylist, num):
+    for row in anylist:
+        for i in reversed(range(len(row))):
+            if row[i] == num:
+                [row.pop(i) for row in anylist]
+    print(anylist)
+
+
+
+mylist = [[1, 2, 3], [4, 5, 1], [1, 6, 7]]
+delcolumn(mylist, 1)
+
+# 5.Реализовать функциональность, которая бы “сворачивала” и “разворачивала” символы табуляции в файле.
+# То есть, на вход передается файл, необходимо заменить все символы табуляции на четыре пробела,
+# либо же заменить все комбинации из четырех символов пробела на символ табуляции (в зависимости от опции,
+# указанной пользователем).
+
+def tab(filename):
+    with open(filename) as fileobject:
+        lines = fileobject.readlines()
+    mystring = ''
+    for line in lines:
+        mystring += line.replace('  ', '    ')
+        # mystring.replace('\t', '    ')
+    print(mystring)
+
+def wspace(filename):
+    with open(filename) as fileobject:
+        lines = fileobject.readlines()
+    mystring = ''
+    for line in lines:
+        mystring += line.replace('    ', '  ')
+        # mystring.replace('\t', '    ')
+    print(mystring)
+
+# tab('hw6.txt')
+user_option = input("Пожалуйста введите номер опции(1 - заменить табуляцию на пробелы, 2 - заменить пробелы на табуляцию): ")
+if user_option == "1":
+    tab('hw6.txt')
+elif user_option == "2":
+    wspace('hw6.txt')
+else:
+    print(f"Вы ввели не допустимое значение.")
+
+
+
+
+
+
+
