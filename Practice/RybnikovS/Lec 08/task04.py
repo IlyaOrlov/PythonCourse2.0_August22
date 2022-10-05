f84c95a3afc196bb4f80152be27232e62a5e1c35
Sergey Rybnikov <kuduzo694@gmail.com>
2022-10-05 22:48:00 +0300

    Задание 4 лекции 8. убран комментарий

--- Practice/RybnikovS/Lec 08/task04.py
+++ Practice/RybnikovS/Lec 08/task04.py
@@ -6,7 +6,7 @@ print(*x)
 
 x = itertools.filterfalse(lambda x: len(x) < 5, ['hello', 'i', 'write', 'cool', 'code'])
 print(*x)
-# Функция выдает на строку 'password' все возможные комбинации вида ([('p', 'a', 's', 's'), ('p', 'a', 's', 'w'), ('p', 'a', 's', 'o'), ...)
+
 str = 'password'
 x = itertools.combinations(str, 4)
 print(*x)
