632d6a94096f5240ccad8da606badcf8e3812a42
Sergey Rybnikov <kuduzo694@gmail.com>
2022-10-06 23:49:32 +0300

    Задание 1 лекция 8

--- Practice/RybnikovS/Lec 09/task01.py
+++ Practice/RybnikovS/Lec 09/task01.py
@@ -1,6 +1,33 @@
 import re
 
-pattern = r"git \w+ [-]\w+"
+# pattern = re.compile(r"git clone https.*\.git[\"]")
+# pattern = re.compile(r"git push")
+
+# pattern = re.compile(r"git [a-z]{3,5} (?:--set-upstream origin .*|\w* \w*|.*.py|.*/.*.git\")")
+
+
+pattern = re.compile(r"git [a-z]{3,8} (?:(?:|--set-upstream\s)origin .*|.*.py|.*/.*.git\"|"
+                     r"\w*@.*.git |-\w (?:\w+|\".*\"))")
+
+
+# pattern = re.compile(r"git [a-z]{3,5} (?:|--set-upstream\s)origin .*")
+# pattern = re.compile(r"git [a-z]{3,5} ")
+# pattern = re.compile(r"git (?:push --set-upstream origin my_branch"
+#                                 r"|pull .* .*"
+#                                 r"|(?:commit|checkout) -\w (?:\w+|\".*\")"
+#                                 r"|add .*.py|push)")
+
+
+# pattern = re.compile(r"git (?:(?:\w{4} --set-upstream origin my_branch|push)"
+#                                 r"|pull .* .*"
+#                                 r"|(?:commit|checkout) -\w (?:\w+|\".*\")"
+#                                 r"|add .*.py)")
+
+# pattern = re.compile(r"git (?:push"
+#                                 r"|pull .* .*"
+#                                 r"|(?:commit|checkout) -\w (?:\w+|\".*\")"
+#                                 r"|add .*.py)")
+
 with open(r"../../README.md", encoding="utf") as file:
     f = file.read()
 print(f)
