37a46b2595f2d001a175447120d2c6a358fd8c27
Dmitry <moisdima@gmail.com>
2022-10-06 21:34:53 +0300

    исправил

--- Practice/MoiseevD/lesson_4_8.py
+++ Practice/MoiseevD/lesson_4_8.py
@@ -6,32 +6,27 @@ game_list = ("камень", "ножницы", "бумага")
 while True:
     game = random.choice(game_list)
     s = input('Введите слово "КАМЕНЬ", "НОЖНИЦЫ", или "БУМАГА": ')
-    if s.lower() == 'камень':
+    s = s.lower()
+    if s == 'камень':
         if game == game_list[0]:
             print(f'---{game}---\n Ничья')
-    if s.lower() == 'камень':
-        if game == game_list[1]:
+        elif game == game_list[1]:
             print(f'---{game}---\n Вы выиграли')
-    if s.lower() == 'камень':
-        if game == game_list[2]:
+        else:
             print(f'---{game}---\n Вы проиграли')
 
-    if s.lower() == 'ножницы':
+    elif s == 'ножницы':
         if game == game_list[0]:
             print(f'---{game}---\n Вы проиграли')
-    if s.lower() == 'ножницы':
-        if game == game_list[1]:
+        elif game == game_list[1]:
             print(f'---{game}---\n Ничья')
-    if s.lower() == 'ножницы':
-        if game == game_list[2]:
+        else:
             print(f'---{game}---\n Вы выиграли')
 
-    if s.lower() == 'бумага':
+    elif s.lower() == 'бумага':
         if game == game_list[0]:
             print(f'---{game}---\n Вы выиграли')
-    if s.lower() == 'бумага':
-        if game == game_list[1]:
+        elif game == game_list[1]:
             print(f'---{game}---\n Вы проиграли')
-    if s.lower() == 'бумага':
-        if game == game_list[2]:
+        else:
             print(f'---{game}---\n Ничья')
