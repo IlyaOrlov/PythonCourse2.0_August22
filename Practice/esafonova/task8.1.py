af236ca5456d3d39f8cf58d942dcde314499079c
Safonova Ekaterina <doinikova.ekaterina@yandex.ru>
2022-10-06 16:17:05 +0300

    update files lec.8.1

--- Practice/esafonova/task8.1.py
+++ Practice/esafonova/task8.1.py
@@ -1,8 +1,8 @@
 #Реализовать итератор, который бы "читал" заданный текст по параграфам. Символ параграфа задается отдельно.
 class Iterator:
 
-    def __init__(self, f, simbol):
-        self._f = f
+    def __init__(self, name_file, simbol):
+        self._f = open(name_file,'r')
         self._simbol = simbol
 
     def __iter__(self):
@@ -11,17 +11,25 @@ class Iterator:
     def __next__(self):
         a = " "
         b = ""
-        while a != "":
-            a = self._f.read(1)
-            if a == self._simbol:
-                break
-            else:
-                b += a
-        return b
+        try:
+            while a != "":
+                a = self._f.read(1)
+                if a == self._simbol:
+                    break
+                else:
+                    b += a
+            return b
+        except StopIteration:
+            pass
 
-with open("text") as f:
-    text = Iterator(f, "@")
-    print(next(text))
-    print(next(text))
-    print(next(text))
-    print(next(text))
+    def __del__(self):
+        self._f.close()
+
+
+
+text = Iterator("text","@")
+print(next(text))
+print(next(text))
+print(next(text))
+print(next(text))
+
