ed9816cb7250dd49383bff934f295b66f6fd138f
Sergey Rybnikov <kuduzo694@gmail.com>
2022-10-07 23:46:11 +0300

    Убраны комментарии задания 1 лек 9

--- Practice/RybnikovS/Lec 09/task01.py
+++ Practice/RybnikovS/Lec 09/task01.py
@@ -3,7 +3,6 @@ import re
 pattern = re.compile(r"git [a-z]{3,8} (?:(?:|--set-upstream\s)origin .*|.*.py|"
                      r"(?:https|.*@)[a-zA-Z0-9_/./:]*\.git"
                      r"|-\w (?:\w+|\".*\"))")
-'(git [^а-я"\n)]+("[а-яА-Я ]+")?)'
 
 with open(r"../../README.md", encoding="utf") as file:
     f = file.read()
