876e19ba364c36d6b1d2277e7649e0273e86528a
Sergey Rybnikov <kuduzo694@gmail.com>
2022-10-14 14:30:39 +0300

    Задание 2 - изменил функцию. Задание 3 - сделал поправки в функции и новый вывод на основании ваших комментариев

--- Practice/RybnikovS/Lec11/task02.py
+++ Practice/RybnikovS/Lec11/task02.py
@@ -1,14 +1,28 @@
 from multiprocessing import Pool
 
 
-def sum_smth(lst):
-    return lst[0] + lst[1]
+def sum_smth(inner_list):
+    a = inner_list[0]
+    match a:
+        case list(a):
+            res = []
+            for i in inner_list:
+                res += i
+        case str(a):
+            res = ""
+            for i in inner_list:
+                res += i
+        case int(a):
+            res = 0
+            for i in inner_list:
+                res += i
+    return res
 
 
 if __name__ == "__main__":
-    lst = ([1, 3],
+    lst = ([1, 3, 5],
            ["abc", "def"],
-           [[1, 2, 3], [4, 5, 6]])
+           [[1, 2, 3, 10], [4, 5, 6]])
     pool = Pool(processes=4)
     res = pool.map(sum_smth, lst)
     print(res)
