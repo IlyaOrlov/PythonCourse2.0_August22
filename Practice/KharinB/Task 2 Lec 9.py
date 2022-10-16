ded173966dca0745ac6f80b9bf4f8e07c863be56
Bogdan <kharin.bogdan-93@yandex.ru>
2022-10-16 18:25:17 +0300

    Сделал, вроде работает... Но как мне кажется слишком намудрил...

--- Practice/KharinB/Task 2 Lec 9.py
+++ Practice/KharinB/Task 2 Lec 9.py
@@ -1,14 +1,13 @@
 import datetime as dt
 
-
 class DataError(Exception):
     pass
-
 
 def vvod(s, n, d):
     try:
         data = dt.datetime.strptime((input(s) or d), "%d/%m/%Y")
-        res = (data, dt.datetime.isoweekday(data))
+        day = dt.datetime.isoweekday(data)
+        res = (data, day)
     except ValueError:
         if n < 3:
             print("Введен некорректный формат даты, введите дату формата: День/Месяц/Год")
@@ -18,11 +17,11 @@ def vvod(s, n, d):
             raise DataError
     return res
 
-def calculator_days():
-    start = vvod("Введите дату начала отсчёта: ", 0, "10/9/2022")
+def calculator_days(s = "18/9/2022", f = "22/10/2022" ):
     a = True
+    start = vvod("Введите дату начала отсчёта(День/Месяц/Год): ", 0, s)
     while a:
-        finish = vvod("Введите дату окончания отсчёта: ", 0, "28/10/2022")
+        finish = vvod("Введите дату окончания отсчёта(День/Месяц/Год): ", 0, f)
         if finish[0] < start[0]:
             print("Введена некорректная дата окончания отсчёта")
             print(f"Дата окончания отсчёта должна быть позже чем: {start[0]}")
@@ -30,18 +29,25 @@ def calculator_days():
         a = False
 
     else:
-        res = (finish[0] - start[0]).days - (7-start[1]) - finish[1]
+        start_m = start[0] - dt.timedelta(days=start[1])
+        finish_m = finish[0] - dt.timedelta(days=finish[1])
+        if start[1]>4:
+            if finish[1]>4:
+                delta = 0
+            else:
+                delta = finish[1]-5
+        else:
+            delta = finish[1]-start[1]
+        if start_m == finish_m:
+            res = finish[1] - start[1]
+        else:
+            res = ((finish_m - start_m) - (finish_m - start_m)/7*2).days - delta
+        return res
 
-        res =  res - res/7*2 + (-1 if start[1]>5 else 5 - start[1]) + (0 if finish[1]>5 else finish[1])
-        return int(res)
-
-# В строчке 36 в первом if мы возвращаем остаток рабочей недели, если начало отсчёта пришлось на середину рабочей недели, включая день старта отсчёта
-# И возвращаем 0, если начало отсчёта пришлось на выходные (то есть не берём день начала отсчёта).
-# Во втором if возвращаем 0, если конец отсчёта пришёлся на выходные
-# и возвращаем количество прошедших дней на неделе, если конец отсчета пришёлся на середину рабочей недели
-# ВКЛЮЧАЯ день окончания отсчёта.
 # По крайней мере именно так считает сайт, которым я регулярно пользуюсь: https://fincalculator.ru/kalkulyator-dnej
 
 
-
-print(calculator_days())
+print(calculator_days()) #25
+print(calculator_days("19/10/2022", "22/10/2022")) #3
+print(calculator_days("16/10/2022", "22/10/2022")) #5
+print(calculator_days("16/10/2021", "22/10/2022")) #265
