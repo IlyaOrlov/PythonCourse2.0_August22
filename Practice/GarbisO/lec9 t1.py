456fa98345d5c83d051dc21f5871367c7f29f0e2
Chipmunk77 <o.garbis@yandex.ru>
2022-10-18 15:55:00 +0300

    Добрый день! Исправила расширение файла READ. И убрала вывод пустого результата

--- Practice/GarbisO/lec9 t1.py
+++ Practice/GarbisO/lec9 t1.py
@@ -5,4 +5,5 @@ with open("README.md", "r") as f:
     pattern = re.compile(r"git [a-z]+\w\b")
     for line in f:
         res = re.findall(pattern, line)
-        print(res)
+        if res != []:
+            print(res)
