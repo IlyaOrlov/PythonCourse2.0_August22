f5690d79ce033ce97fdbe197369f5db5b5013fe8
Chipmunk77 <o.garbis@yandex.ru>
2022-10-18 15:50:55 +0300

    Добрый день! Исправила расширение файла READ

--- Practice/GarbisO/lec9 t1.py
+++ Practice/GarbisO/lec9 t1.py
@@ -1,7 +1,7 @@
 import re
 
 
-with open("README.txt", "r") as f:
+with open("README.md", "r") as f:
     pattern = re.compile(r"git [a-z]+\w\b")
     for line in f:
         res = re.findall(pattern, line)
